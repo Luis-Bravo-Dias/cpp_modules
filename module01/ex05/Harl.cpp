2c7fad527ed21c5964fb65e1fb8e6bfc488de9eb
Luis miguel Leiria bravo rodrigues dias <lleiria-@c1r20s6.42lisboa.com>
2023-04-26 18:02:06 +0100

    16

--- module01/ex05/Harl.cpp
+++ module01/ex05/Harl.cpp
@@ -6,44 +6,50 @@
 /*   By: lleiria- <lleiria-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 16:35:43 by lleiria-          #+#    #+#             */
-/*   Updated: 2023/04/25 17:00:06 by lleiria-         ###   ########.fr       */
+/*   Updated: 2023/04/26 18:01:11 by lleiria-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
-Harl::Harl(){};
+Harl::Harl()
+{
+	funs[0].level = "DEBUG";
+	funs[0].fun = &Harl::_debug;
+	funs[1].level = "INFO";
+	funs[1].fun = &Harl::_info;
+	funs[2].level = "WARNING";
+	funs[2].fun = &Harl::_warning;
+	funs[3].level = "ERROR";
+	funs[3].fun = &Harl::_error;
+	
+};
 
 Harl::~Harl(){};
 
 void	Harl::complain(std::string level)
 {
-	if (level == "DEBUG")
-		this->_debug();
-	else if (level == "INFO")
-		this->_info();
-	else if (level == "WARNING")
-		this->_warning();
-	else if (level == "ERROR")
-		this->_error();
+	for (int i = 0; i < 4; i++)
+		if (this->funs[i].level == level)
+			(this->*funs[i].fun)();
 }
 
-void	Harl::_debug()
+void	Harl::_debug() const
 {
 	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
 }
 
-void	Harl::_info()
+void	Harl::_info() const
 {
 	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
 }
 
-void	Harl::_warning()
+void	Harl::_warning() const
 {
 	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
 }
 
-void	Harl::_error()
+void	Harl::_error() const
 {
 	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
 }
--- module01/ex05/Harl.hpp
+++ module01/ex05/Harl.hpp
@@ -6,7 +6,7 @@
 /*   By: lleiria- <lleiria-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 16:35:21 by lleiria-          #+#    #+#             */
-/*   Updated: 2023/04/25 16:43:22 by lleiria-         ###   ########.fr       */
+/*   Updated: 2023/04/26 17:10:10 by lleiria-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,18 @@ public:
 	void	complain(std::string level);
 
 private:
-	void	_debug(void);
-	void	_info(void);
-	void	_warning(void);
-	void	_error(void);
+	void	_debug(void) const;
+	void	_info(void) const;
+	void	_warning(void) const;
+	void	_error(void) const;
+
+	typedef struct s_funStruct
+	{
+		std::string	level;
+		void	(Harl::*fun)(void) const;
+	}	t_funStruct;
+
+	t_funStruct	funs[4];
 };
 
 #endif
