2133bac88bbe579e409dafb89540e59df38079b8
Luis miguel Leiria bravo rodrigues dias <lleiria-@c1r20s6.42lisboa.com>
2023-04-27 15:37:34 +0100

    01 finished

--- module01/ex05/main.cpp
+++ module01/ex05/main.cpp
@@ -6,7 +6,21 @@
 /*   By: lleiria- <lleiria-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 16:34:27 by lleiria-          #+#    #+#             */
-/*   Updated: 2023/04/25 16:34:28 by lleiria-         ###   ########.fr       */
+/*   Updated: 2023/04/27 14:59:44 by lleiria-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Harl.hpp"
+
+int	main()
+{
+	Harl	harl;
+	
+	harl.complain("DEBUG");
+	harl.complain("INFO");
+	harl.complain("WARNING");
+	harl.complain("ERROR");
+	harl.complain("");
+
+	return (0);
+}
