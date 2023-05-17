365efb47c9c641810067c7c83acc1897ab9ac97e
Luis miguel Leiria bravo rodrigues dias <lleiria-@c1r20s6.42lisboa.com>
2023-05-17 17:48:31 +0100

    21

--- module02/ex02/Fixed.cpp
+++ module02/ex02/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: lleiria- <lleiria-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 15:25:54 by lleiria-          #+#    #+#             */
-/*   Updated: 2023/05/16 17:59:50 by lleiria-         ###   ########.fr       */
+/*   Updated: 2023/05/17 17:42:03 by lleiria-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,11 +69,125 @@ Fixed	Fixed::operator/(Fixed const & rhs) const
 	return (Fixed(this->_fpNbr / rhs.getRawBits()));
 }
 
+int	Fixed::operator>(Fixed const & rhs) const
+{
+	if (this->_fpNbr > rhs.getRawBits())
+		return (1);
+	else
+		return (0);
+}
+
+int	Fixed::operator<(Fixed const & rhs) const
+{
+	if (this->_fpNbr < rhs.getRawBits())
+		return (1);
+	else
+		return (0);
+}
+
+int	Fixed::operator>=(Fixed const & rhs) const
+{
+	if (this->_fpNbr >= rhs.getRawBits())
+		return (1);
+	else
+		return (0);
+}
+
+int	Fixed::operator<=(Fixed const & rhs) const
+{
+	if (this->_fpNbr <= rhs.getRawBits())
+		return (1);
+	else
+		return (0);
+}
+
+int	Fixed::operator==(Fixed const & rhs) const
+{
+	if (this->_fpNbr == rhs.getRawBits())
+		return (1);
+	else
+		return (0);
+}
+
+int	Fixed::operator!=(Fixed const & rhs) const
+{
+	if (this->_fpNbr != rhs.getRawBits())
+		return (1);
+	else
+		return (0);
+}
+
 int	Fixed::getRawBits() const
 {
 	// std::cout << "getRawBits member function called" << std::endl;
 
 	return (this->_fpNbr);
+}
+
+Fixed & Fixed::operator++()
+{
+	this->_fpNbr++;
+	return (*this);
+}
+
+Fixed Fixed::operator++(int)
+{
+	Fixed	newFix(*this);
+	this->_fpNbr++;
+	return (newFix);
+}
+
+Fixed & Fixed::operator--()
+{
+	this->_fpNbr--;
+	return (*this);
+}
+
+Fixed Fixed::operator--(int)
+{
+	Fixed	newFix(*this);
+	this->_fpNbr--;
+	return (newFix);
+}
+
+Fixed & Fixed::min(Fixed &a, Fixed &b)
+{
+	if (a.getRawBits() < b.getRawBits())
+		return (a);
+	else if (a.getRawBits() > b.getRawBits())
+		return (b);
+	else
+		return ;
+}
+
+Fixed const & Fixed::min(Fixed const &a, Fixed const &b)
+{
+	if (a.getRawBits() < b.getRawBits())
+		return (a);
+	else if (a.getRawBits() > b.getRawBits())
+		return (b);
+	else
+		return ;
+}
+
+Fixed & Fixed::max(Fixed &a, Fixed &b)
+{
+	if (a.getRawBits() > b.getRawBits())
+		return (a);
+	else if (a.getRawBits() < b.getRawBits())
+		return (b);
+	else
+		return ;
+}
+
+Fixed const & Fixed::max(Fixed const &a, Fixed const &b)
+{
+	if (a.getRawBits() > b.getRawBits())
+		return (a);
+	else if (a.getRawBits() < b.getRawBits())
+		return (b);
+	else
+		return ;
 }
 
 void	Fixed::setRawBits(int const & raw)
--- module02/ex02/Fixed.hpp
+++ module02/ex02/Fixed.hpp
@@ -6,42 +6,58 @@
 /*   By: lleiria- <lleiria-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 15:25:14 by lleiria-          #+#    #+#             */
-/*   Updated: 2023/05/16 17:53:26 by lleiria-         ###   ########.fr       */
+/*   Updated: 2023/05/17 17:40:40 by lleiria-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FIXED_HPP
-# define FIXED_HPP
+#define FIXED_HPP
 
-# include <iostream>
-# include <cmath>
+#include <iostream>
+#include <cmath>
 
 class Fixed
 {
-	public:
-		Fixed(void);
-		Fixed(int const & nbr);
-		Fixed(float const & nbr);
-		Fixed(Fixed const & src);
-		~Fixed(void);
-		
-		Fixed & operator=(Fixed const & rhs);
-		Fixed	operator+(Fixed const & rhs) const;
-		Fixed	operator-(Fixed const & rhs) const;
-		Fixed	operator*(Fixed const & rhs) const;
-		Fixed	operator/(Fixed const & rhs) const;
+public:
+	Fixed(void);
+	Fixed(int const &nbr);
+	Fixed(float const &nbr);
+	Fixed(Fixed const &src);
+	~Fixed(void);
 
-		
-		int	getRawBits(void) const;
-		void setRawBits(int const & raw);
-		float	toFloat(void) const;
-		int	toInt(void) const;
-	
-	private:
-		int	_fpNbr;
-		static const int	_bits = 8;
+	Fixed &operator=(Fixed const &rhs);
+	Fixed operator+(Fixed const &rhs) const;
+	Fixed operator-(Fixed const &rhs) const;
+	Fixed operator*(Fixed const &rhs) const;
+	Fixed operator/(Fixed const &rhs) const;
+
+	int operator>(Fixed const &rhs) const;
+	int operator<(Fixed const &rhs) const;
+	int operator>=(Fixed const &rhs) const;
+	int operator<=(Fixed const &rhs) const;
+	int operator==(Fixed const &rhs) const;
+	int operator!=(Fixed const &rhs) const;
+
+	Fixed &operator++(void); //pre-increment
+	Fixed operator++(int); //post-increment
+	Fixed &operator--(void);
+	Fixed operator--(int);
+
+	Fixed &min(Fixed &a, Fixed &b);
+	Fixed const &min(Fixed const &a, Fixed const &b);
+	Fixed &max(Fixed &a, Fixed &b);
+	Fixed const &max(Fixed const &a, Fixed const &b);
+
+	int getRawBits(void) const;
+	void setRawBits(int const &raw);
+	float toFloat(void) const;
+	int toInt(void) const;
+
+private:
+	int _fpNbr;
+	static const int _bits = 8;
 };
 
-std::ostream &	operator<<(std::ostream & o, Fixed const & rhs);
+std::ostream &operator<<(std::ostream &o, Fixed const &rhs);
 
 #endif
