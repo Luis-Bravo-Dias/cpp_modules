1bf5557ec5cd8263b492d98878699888e7d5ffb3
Luis miguel Leiria bravo rodrigues dias <lleiria-@c2r10s5.42lisboa.com>
2023-08-01 17:01:11 +0100

    43

--- module05/ex02/ShrubberyCreationForm.cpp
+++ module05/ex02/ShrubberyCreationForm.cpp
@@ -6,7 +6,36 @@
 /*   By: lleiria- <lleiria-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/31 10:58:34 by lleiria-          #+#    #+#             */
-/*   Updated: 2023/07/31 10:58:35 by lleiria-         ###   ########.fr       */
+/*   Updated: 2023/08/01 16:56:43 by lleiria-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ShrubberyCreationForm.hpp"
+
+ShrubberyCreationForm::ShrubberyCreationForm(std::string target): _target(target), AForm("Shrubbery", 145, 137)
+{
+	std::cout << "Default ShrubberyCreationForm constructor called" << std::endl;
+}
+
+ShrubberyCreationForm::ShrubberyCreationForm(ShrubberyCreationForm const &src): AForm(src)
+{
+	std::cout << "Copy ShrubberyCreationForm constructor called" << std::endl;
+	*this = src;
+}
+
+ShrubberyCreationForm::~ShrubberyCreationForm()
+{
+	std::cout << "ShrubberyCreationForm destructor called" << std::endl;
+}
+
+ShrubberyCreationForm & ShrubberyCreationForm::operator=(ShrubberyCreationForm const &rhs)
+{
+	if (this != &rhs)
+		this->_target = rhs._target;
+	return (*this);
+}
+
+void ShrubberyCreationForm::execute(Bureaucrat const & executer)
+{
+	
+}
--- module05/ex02/ShrubberyCreationForm.hpp
+++ module05/ex02/ShrubberyCreationForm.hpp
@@ -6,7 +6,7 @@
 /*   By: lleiria- <lleiria-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/31 10:57:53 by lleiria-          #+#    #+#             */
-/*   Updated: 2023/07/31 12:17:26 by lleiria-         ###   ########.fr       */
+/*   Updated: 2023/08/01 16:19:22 by lleiria-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,27 @@
 
 class ShrubberyCreationForm : public AForm
 {
+	public:
+		ShrubberyCreationForm(std::string target);
+		ShrubberyCreationForm(ShrubberyCreationForm const &src);
+		virtual ~ShrubberyCreationForm(void);
+
+		ShrubberyCreationForm &operator=(ShrubberyCreationForm const &rhs);
+
+		virtual void execute(Bureaucrat const & executer);
+
+		class ExecGradeException :public std::exception
+		{
+			public:
+				virtual const char* what() const throw()
+				{
+					return ("Invalid grade to execute");
+				}
+		};
 	
-}
+	private:
+		ShrubberyCreationForm(void);
+		std::string	_target;
+};
 
 #endif
