c231985a47b9317f7b18f2b5ccf6dd1c562cc807
Luis miguel Leiria bravo rodrigues dias <lleiria-@c2r10s5.42lisboa.com>
2023-09-05 16:45:21 +0100

    56

--- module07/ex01/main.cpp
+++ module07/ex01/main.cpp
@@ -6,19 +6,37 @@
 /*   By: lleiria- <lleiria-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/04 16:18:19 by lleiria-          #+#    #+#             */
-/*   Updated: 2023/09/04 16:20:35 by lleiria-         ###   ########.fr       */
+/*   Updated: 2023/09/05 11:40:50 by lleiria-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 
-int main() {
-    int intArray[] = {1, 2, 3, 4, 5};
-    // double doubleArray[] = {1.1, 2.2, 3.3, 4.4, 5.5};
-    // char charArray[] = {'H', 'e', 'l', 'l', 'o'};
+void printStr(std::string str)
+{
+	std::cout << str << std::endl;
+}
 
-    std::cout << "Integer array: ";
-    iter(intArray, 5, printArray);
+void printten(int n)
+{
+	std::cout << n * 10 << std::endl;
+}
+
+void printcap(char c)
+{
+	std::cout << static_cast<char>(c - ('a' - 'A')) << std::endl;
+}
+
+int main()
+{
+    std::string test[] = {"zero", "um", "dois", "tres", "quatro"};
+	iter(test, 5, printStr);
+
+    int test2[] = {1,2,3,4,5,6,7,8,9};
+	iter(test2, 9, printten);
+
+    char test3[] = {'a','z','b','p','r','y','u','f','g'};
+	iter(test3, 9, printcap);
 
     return 0;
 }
--- module07/ex02/Array.hpp
+++ module07/ex02/Array.hpp
@@ -1,25 +1,45 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   iter.hpp                                           :+:      :+:    :+:   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lleiria- <lleiria-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/04 15:56:00 by lleiria-          #+#    #+#             */
-/*   Updated: 2023/09/05 15:37:21 by lleiria-         ###   ########.fr       */
+/*   Created: 2023/09/05 15:33:36 by lleiria-          #+#    #+#             */
+/*   Updated: 2023/09/05 16:41:37 by lleiria-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef ITER_HPP
-#define ITER_HPP
+#ifndef ARRAY_HPP
+#define ARRAY_HAPP
 
-#include <iostream>
+# include <iostream>
 
-template<typename T>
-void iter(T *array, int length, void fun(T param))
+template <typename T>
+class Array
 {
-	for (int i = 0; i < length; i++)
-		fun(array[i]);
+	public:
+		Array(void);
+		Array(Array const &src);
+		~Array(void);
+
+		Array &operator=(Array const &rhs);
+		
+		T *Construction(void)
+		{
+			new T *array;
+			return (array);
+		}
+		
+		T *Construction(unsigned int n)
+		{
+			new T array[n];
+			return (array);
+		}
+
+	private:
+		
 };
+
 
 #endif
