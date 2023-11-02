4d204bfd63370fff26c77de2eb2bcdacddf3e6b2
Luis miguel Leiria bravo rodrigues dias <lleiria-@c1r20s6.42lisboa.com>
2023-11-02 17:06:21 +0000

    59

--- module09/ex00/BitcoinExchange.hpp
+++ module09/ex00/BitcoinExchange.hpp
@@ -6,7 +6,16 @@
 /*   By: lleiria- <lleiria-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 16:30:05 by lleiria-          #+#    #+#             */
-/*   Updated: 2023/11/01 16:30:06 by lleiria-         ###   ########.fr       */
+/*   Updated: 2023/11/02 17:03:47 by lleiria-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef BITCOINEXCHANGE_HPP
+#define BITCOINEXCHANGE_HPP
+
+# include <iostream>
+# include <fstream>
+
+int not_valid(std::ifstream input);
+
+#endif
---
+++ module09/ex00/main.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lleiria- <lleiria-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/02 12:43:49 by lleiria-          #+#    #+#             */
+/*   Updated: 2023/11/02 17:03:40 by lleiria-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "BitcoinExchange.hpp"
+
+int main(int ac, char** av)
+{
+    if (ac != 2)
+        std::cout << "Wrong number of paramters" << std::endl;
+    else
+    {
+        std::ifstream input;
+        input.open(av[1]);
+        if (input.is_open())
+        {
+            if (not_valid(input))
+            {
+                std::cout << "Input file not valid" << std::endl;
+                return (0);
+            }
+        }
+        input.close();
+    }
+    return (0);
+}
