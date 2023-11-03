fe392d0901968ab75b3abe8f6766550215c8262e
Luis miguel Leiria bravo rodrigues dias <lleiria-@c1r20s6.42lisboa.com>
2023-11-03 17:19:12 +0000

    60

--- module09/ex00/BitcoinExchange.cpp
+++ module09/ex00/BitcoinExchange.cpp
@@ -6,13 +6,23 @@
 /*   By: lleiria- <lleiria-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 16:29:53 by lleiria-          #+#    #+#             */
-/*   Updated: 2023/11/02 17:04:12 by lleiria-         ###   ########.fr       */
+/*   Updated: 2023/11/03 17:02:48 by lleiria-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
 
-int not_valid(std::ifstream input)
+int not_valid(std::ifstream &input)
 {
-    return (1);
+    std::string line;
+    std::getline(input, line);
+    while (std::getline(input, line))
+    {
+        std::string year = line.substr(0, 4);
+        std::string month = line.substr(5, 2);
+        std::string day = line.substr(8, 2);
+        // for (int i = 0; line[i]; i++)
+        //     std::cout << "teste : " << line[i] << std::endl;
+    }
+    return (0);
 }
--- module09/ex00/BitcoinExchange.hpp
+++ module09/ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: lleiria- <lleiria-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 16:30:05 by lleiria-          #+#    #+#             */
-/*   Updated: 2023/11/02 17:03:47 by lleiria-         ###   ########.fr       */
+/*   Updated: 2023/11/03 12:19:55 by lleiria-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@
 # include <iostream>
 # include <fstream>
 
-int not_valid(std::ifstream input);
+int not_valid(std::ifstream &input);
 
 #endif
